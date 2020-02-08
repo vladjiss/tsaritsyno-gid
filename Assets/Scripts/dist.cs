fba9c8de4a3eda5b31a187883720842610231e93
vladjiss <danreegly@gmail.com>
2020-02-08 23:47:11 +0300

    dist script

---
+++ Assets/Scripts/dist.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dist : MonoBehaviour
+{
+    public string str;
+    public Transform cam;
+    public TextMesh textMesh;
+    float d = 0f;
+    void Update()
+    {
+        d = Vector3.Distance(cam.position, transform.position);
+        textMesh.text = str + string.Format("{0:0.##}", d) + "m";
+    }
+}
