f24db62a89bcfd2df3ba985921582a1d7f9edb5e
vladjiss <danreegly@gmail.com>
2020-02-08 17:14:14 +0300

    add code for adding outdoor information about objects in ar

--- Assets/Scripts/poser.cs
+++ Assets/Scripts/poser.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class poser : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int counter = 0;
+    public Transform[] objs;
+    public Transform[] objs_pop;
+    public Camera cam;
+    // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchSupported && Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonUp(0)  && !Input.touchSupported)
+        {
+            Vector3 v = cam.transform.forward;
+            if (Input.touchSupported)
+                v = cam.ScreenPointToRay(Input.GetTouch(0).position).direction;
+            else
+                v = cam.ScreenPointToRay(Input.mousePosition).direction;
+            Vector3 pos = cam.transform.position + v * 10;
+            Quaternion rot = cam.transform.rotation;
+            if (counter < objs.Length)
+            {
+                //Instantiate(objs[counter], pos, rot);
+                objs[counter].gameObject.SetActive(true);
+                objs[counter].position = pos;
+                objs[counter].rotation = rot;
+            }
+            else
+                objs_pop[counter - objs.Length].gameObject.SetActive(true);
+            counter++;
+        }
     }
+
 }
