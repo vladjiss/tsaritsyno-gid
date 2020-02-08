e1bf972b70c77827dad60b6c63eb0dad19ae6418
vladjiss <danreegly@gmail.com>
2020-02-08 22:59:23 +0300

    add ibeacon simulation, interactive switch betwen map and ar

---
+++ Assets/Scripts/ibeacon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ibeacon : MonoBehaviour
+{
+    float t =0f;
+    public Material m;
+    public Color color1, color2;
+    bool st = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (st)
+        {
+            t += Time.deltaTime*2f;
+            if (t > 1)
+            {
+                t = 1f;
+                st = !st;
+            }
+        }
+        else
+        {
+            t -= Time.deltaTime * 2f;
+            if (t < 0)
+            {
+                t = 0f;
+                st = !st;
+            }
+        }
+        m.SetColor("_TintColor", Color.Lerp(color1, color2, t));
+    }
+}
---
+++ Assets/Scripts/interactive_switch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactive_switch : MonoBehaviour
+{
+    public Transform cam, map;
+    bool map_on = true;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(cam.localEulerAngles.x);
+        if (cam.localEulerAngles.x > 45f && cam.localEulerAngles.x <270 && !map_on)
+        {
+            map_on = true;
+            Switch();
+        }
+        if ((cam.localEulerAngles.x <= 45f || cam.localEulerAngles.x >= 270) && map_on)
+        {
+            map_on = false;
+            Switch();
+        }
+    }
+    void Switch()
+    {
+        if (map_on)
+        {
+            map.gameObject.SetActive(true);
+        }
+        else
+            map.gameObject.SetActive(false);
+    }
+}
