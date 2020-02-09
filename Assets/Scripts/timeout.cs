bdffd50e8e50c7835b527c522e4cace6768ab586
vladjiss <danreegly@gmail.com>
2020-02-09 05:13:20 +0300

    add mobile UI and admin panel

---
+++ Assets/Scripts/timeout.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeout : MonoBehaviour
+{
+    public GameObject gb;
+    float timer = 0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 3f)
+        {
+            gb.SetActive(true);
+        }
+    }
+}
